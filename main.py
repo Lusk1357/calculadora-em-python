453541b5213f8afbabc937b3f3afe48514ea6777
unknown <nunes.lucas1357@gmail.com>
2026-05-19 14:58:22 -0300

    adicionando codigo main

---
+++ main.py
@@ -0,0 +1,30 @@
+# Calculadora simples em Python
+
+print("=== CALCULADORA ===")
+
+# Entrada de dados
+num1 = float(input("Digite o primeiro número: "))
+operacao = input("Digite a operação (+, -, *, /): ")
+num2 = float(input("Digite o segundo número: "))
+
+# Verificação da operação
+if operacao == "+":
+    resultado = num1 + num2
+
+elif operacao == "-":
+    resultado = num1 - num2
+
+elif operacao == "*":
+    resultado = num1 * num2
+
+elif operacao == "/":
+    if num2 != 0:
+        resultado = num1 / num2
+    else:
+        resultado = "Erro: divisão por zero"
+
+else:
+    resultado = "Operação inválida"
+
+# Saída
+print("Resultado:", resultado)
